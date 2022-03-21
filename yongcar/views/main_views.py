f6feb321d74e1a615e36ce2395c7f4ce5405d130
unknown <joyw93@naver.com>
2022-03-21 13:38:38 +0900

    predict 함수 수정

--- yongcar/views/main_views.py
+++ yongcar/views/main_views.py
@@ -8,7 +8,7 @@ def predict():
      args = request.json
      model = args['model']
      age = int(args['age'])
-     odo = int(args['odo'])
+     odo = int(args['odo'].replace(',', ''))
      color = args['color']
      fuel = args['fuel']
      price = predict_price(model, age, odo, fuel, color)
